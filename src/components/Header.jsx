78a5f99a19bdcbd0ad755ca62b3b12c345e43a6a
Annica Westford <annica.westford@gmail.com>
2023-05-31 12:43:05 +0200

    Fix UI and logic for Home page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,31 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { NavLink, Link } from "react-router-dom";
 import logo from "../images/logo color.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
 
 function Header() {
+  const [currentPage, setCurrentPage] = useState("home");
   return (
     <div className="header-container">
       <Link to="/">
         <img style={{ maxWidth: "120px" }} src={logo} alt="bun drop logo" />
       </Link>
       <div className="header-links-container">
-        <Link to="/">Hem</Link>
-        <Link to="/menu">Meny</Link>
-        <Link to="/order">Beställ</Link>
-        <Link to="/order">
-          <FontAwesomeIcon icon={faCartShopping} />
-        </Link>
+        <NavLink exact to="/" activeClassName="active">
+          <p className="header-link">Hem</p>
+        </NavLink>
+        <NavLink exact to="/menu" activeClassName="active">
+          <p className="header-link">Meny</p>
+        </NavLink>
+        <NavLink exact to="/order" activeClassName="active">
+          <div className="header-link flex-container">
+            <p>Beställ</p>
+            <div className="header-link">
+              <FontAwesomeIcon icon={faCartShopping} />
+            </div>
+          </div>
+        </NavLink>
       </div>
     </div>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,35 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
 function Home() {
-  return <div></div>;
+  return (
+    <div className="home-container flex-container">
+      <div>
+        <h1
+          style={{ fontSize: "60px", borderRadius: "80px 80px 10px 10px" }}
+          className="lightest-salmon-color brown-background-home-container"
+        >
+          BUN DROP
+        </h1>
+      </div>
+      <div>
+        <Link to="/menu">
+          <button style={{ marginRight: "5px" }}>Meny</button>
+        </Link>
+        <Link to="/order">
+          <button style={{ marginLeft: "5px" }}>Beställ</button>
+        </Link>
+      </div>
+      <div>
+        <h2
+          style={{ borderRadius: "10px 10px 50px 50px" }}
+          className="lightest-salmon-color brown-background-home-container"
+        >
+          DROP IT LIKE IT'S HOT!
+        </h2>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
